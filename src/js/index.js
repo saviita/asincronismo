11a20ce91afd223e5160310fafb8e5354a2ff372
saviita <taatt2000@gmail.com>
2024-07-16 23:37:31 +0200

    update

--- src/js/index.js
+++ src/js/index.js
@@ -1,2 +1,26 @@
 // El styles lo importamos aquí, ya se carga después al compilar todo
 import '../scss/styles.scss';
+
+const adviceElement = document.getElementById('advice-number')
+const textElement = document.getElementById('card-text')
+const buttonElement = document.getElementById('btn')
+
+const generateText = data => {
+    textElement.textContent = data.slip.advice
+    adviceElement.textContent = data.slip.id  
+}
+
+const getUsers = async url => {
+    try {
+        const response = await fetch(url)
+        const data = await response.json()
+        console.log(data)
+        generateText(data)
+    } catch (error) {
+        console.log(error)
+    }
+}
+
+buttonElement.addEventListener('click', () => {
+    getUsers('https://api.adviceslip.com/advice')
+})
